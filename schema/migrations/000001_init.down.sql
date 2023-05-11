5981b897149ca26450bfa5dd61e2b8ab4a71c0c5
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2023-05-11 10:08:07 +0500

    Add new table for vacancies and new trigger

--- schema/migrations/000001_init.down.sql
+++ schema/migrations/000001_init.down.sql
@@ -1,1 +1,9 @@
-DROP TABLE IF EXISTS tbl_team;
+-- Drop the trigger before dropping the table
+DROP TRIGGER IF EXISTS trg_update_updated_at ON tbl_vacancy;
+
+-- Drop the foreign key constraint before dropping the table
+ALTER TABLE tbl_vacancy DROP CONSTRAINT tbl_vacancy_vacancy_team_id_fkey;
+
+-- Drop the tables
+DROP TABLE tbl_vacancy;
+DROP TABLE tbl_team;
--- schema/migrations/000001_init.up.sql
+++ schema/migrations/000001_init.up.sql
@@ -3,6 +3,34 @@ CREATE TABLE tbl_team (
     team_name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE tbl_vacancy 
+(
+    vacancy_id SERIAL PRIMARY KEY,
+    vacancy_team_id INT NOT NULL,
+    vacany_status_id INT NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    vacancy_description TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL,
+    vacancy_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (vacancy_team_id) REFERENCES tbl_team(team_id)
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_updated_at
+BEFORE UPDATE ON tbl_vacancy
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+
 INSERT INTO tbl_team (team_name) VALUES ('Frontend');
 INSERT INTO tbl_team (team_name) VALUES ('Backend');
 INSERT INTO tbl_team (team_name) VALUES ('DevOps');

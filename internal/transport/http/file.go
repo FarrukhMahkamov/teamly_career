c4dc85ca3f376d03618ae9b7fe41b723edcff26f
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2023-05-13 00:24:33 +0500

    bug fixes and other minor improvments

--- internal/transport/http/file.go
+++ internal/transport/http/file.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"log"
-
 	"github.com/FarrukhMahkamov/teamly_career/pkg"
 	"github.com/gin-gonic/gin"
 )
@@ -10,8 +8,13 @@ import (
 func (h *Handler) FileUpload(c *gin.Context) {
 	Destination := c.Param("destination")
 
-	File, _ := c.FormFile("file")
-	log.Println(File.Filename)
+	File, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "file not found in request",
+		})
+		return
+	}
 
 	//there are 2 types of files: cv and photo
 	FileName, err := pkg.UploadRequestFile(File, Destination)
